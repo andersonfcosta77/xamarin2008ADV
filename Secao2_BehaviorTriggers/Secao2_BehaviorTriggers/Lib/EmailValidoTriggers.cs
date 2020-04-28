34b00b8cd8fa9110044c778eebd4bda2acd7ecac
softecsul <softecsul@softecsul.com>
2020-04-28 07:58:14 -0300

    Adicionado o projeto Secao2_BehaviorTriggers CONCLUÍDO o exemplo de Behavior e INICIADO o de Triggers

---
+++ Secao2_BehaviorTriggers/Secao2_BehaviorTriggers/Lib/EmailValidoTriggers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Secao2_BehaviorTriggers.Lib
+{
+    public class EmailValidoTrigger : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry sender)
+        {
+            if (sender.Text.IndexOf("@") >= 0)
+            {
+                sender.TextColor = Color.Green;
+            }
+            else
+            {
+                sender.TextColor = Color.Red;
+            }
+        }
+    }
