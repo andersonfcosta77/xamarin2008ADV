02c02939c0485fed14ec54a20387a2df6439ce0e
softecsul <softecsul@softecsul.com>
2020-04-29 23:38:36 -0300

    Adiconado o projeto Secao3_ListView e realizado primeira atividade Lista Agrupada

---
+++ Secao3_ListView/Secao3_ListView/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Secao3_ListView.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Secao3_ListView
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            ListaFuncionarios.ItemsSource = GetFuncionarios();
+        }
+
+        private List<Grupo> GetFuncionarios()
+        {
+            return new List<Grupo>
+            {
+                new Grupo("Presidente", "CEO")
+                {
+                    new Pessoa { Nome = "José" }
+                },
+                new Grupo("Diretores", "Dir.")
+                {
+                    new Pessoa { Nome = "João" },
+                    new Pessoa { Nome = "Maria" },
+                },
+                new Grupo("Gerentes", "Ger.")
+                {
+                    new Pessoa { Nome = "Felipe" },
+                    new Pessoa { Nome = "Judas" },
+                },
+            };
+
+            //return new List<Pessoa>
+            //{
+            //    new Pessoa { Nome = "José" },
+            //    new Pessoa { Nome = "João" },
+            //    new Pessoa { Nome = "Maria" },
+            //    new Pessoa { Nome = "Felipe" },
+            //    new Pessoa { Nome = "Judas" },
+            //};
+        }
+    }
+}
---
+++ Secao3_ListView/Secao3_ListView/Model/Grupo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Secao3_ListView.Model
+{
+    class Grupo : List<Pessoa>
+    {
+        public Grupo(string Titulo, String TituloCurto)
+        {
+            this.Titulo = Titulo;
+            this.TituloCurto = TituloCurto;
+        }
+
+        public string Titulo { get; set; }
+        public string TituloCurto { get; set; }
+    }
+}
