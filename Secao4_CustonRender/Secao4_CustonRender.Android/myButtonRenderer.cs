c04a66f232871f762f191011fc8e43c90ea891ab
andersonfc <anderson@softecsul.com>
2020-05-07 17:08:54 -0300

    Adiconado Projeto Secao4_CustomRender e concluido o exemplo

---
+++ Secao4_CustonRender/Secao4_CustonRender.Android/myButtonRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly:ExportRenderer(typeof(Secao4_CustonRender.Custons.myButton),typeof(Secao4_CustonRender.Droid.myButtonRenderer))]
+namespace Secao4_CustonRender.Droid
+{
+    public class myButtonRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+
+                Control.SetBackgroundResource(Resource.Drawable.button_round);
+            }
+        }
+    }
+}
---
+++ Secao4_CustonRender/Secao4_CustonRender/Custons/myButton.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Secao4_CustonRender.Custons
+{
+    public class myButton : Button
+    {
+
+    }
+}
