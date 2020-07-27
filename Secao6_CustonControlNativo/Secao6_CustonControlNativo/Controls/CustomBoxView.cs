4d54b0952bf3f9954685c98d17ab6975ebd76819
andersonfcosta <andersonfcosta77@gmail.com>
2020-07-27 19:23:57 -0300

    Secao6 - Incompleta

--- Secao6_CustonControlNativo/Secao6_CustonControlNativo/Controls/CustomBoxView.cs
+++ Secao6_CustonControlNativo/Secao6_CustonControlNativo/Controls/CustomBoxView.cs
@@ -6,8 +6,7 @@ namespace Secao6_CustonControlNativo.Controls
     public class CustomBoxView : BoxView
     {
         public static readonly BindableProperty EspessuraProperty = BindableProperty.Create<CustomBoxView, double>(
-            p=>p.Espessura, default(double))
-            ;
+            p=>p.Espessura, default(double));
 
         public double Espessura
         {
