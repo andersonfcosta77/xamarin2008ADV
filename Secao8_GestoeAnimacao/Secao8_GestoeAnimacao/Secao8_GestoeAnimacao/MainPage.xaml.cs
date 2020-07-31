174e77740585c1deb570ae047dd796052f8e21de
andersonfcosta <andersonfcosta77@gmail.com>
2020-07-31 15:02:48 -0300

    Secao8_GestaoeAnimacao - Foi iniciada

---
+++ Secao8_GestoeAnimacao/Secao8_GestoeAnimacao/Secao8_GestoeAnimacao/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Secao8_GestoeAnimacao
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+            //Tipos de Gesture
+            // Tap - toque ou clique
+            // Pinch - pinça
+            // Pan - Toque e arrasta
+
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            count++;
+            DisplayAlert("Tapped","Label1 foi tocada " + count + " vezes!1'","Ok");
+        }
+    }
+}
